9f1a1ad43ab4f98225985978d4930ce5057b83a3
Ashwin Rohit <ashwinrohit@proton.me>
2023-04-07 21:41:20 -0700

    Modify files to prep for Vercel Hosting

--- api/index.js
+++ api/index.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const path = require("path");
 const history = require("connect-history-api-fallback");
+const { v4 } = require('uuid');
 const app = express();
-const port = process.env.PORT || 8080;
 
-const buildPath = path.join(__dirname, "..", "build");
-app.use(express.static(buildPath));
 app.use(express.json());
 app.use(history());
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(buildPath, "index.html"));
-});
 
 // Configure email server
 app.post("/send-email", (req, res) => {
@@ -45,3 +36,17 @@ app.post("/send-email", (req, res) => {
 		res.status(500).send("Email not sent");
 	}
 });
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
