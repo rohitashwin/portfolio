ddbdbf2f0fe4a972899756c95616157d382abdc5
Ashwin Rohit <ashwinrohit@proton.me>
2023-04-07 22:06:26 -0700

    Fix vercel config

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const history = require("connect-history-api-fallback");
-const { v4 } = require('uuid');
 const app = express();
-
 app.use(express.json());
-app.use(history());
 
 // Configure email server
-app.post("/send-email", (req, res) => {
+app.post("/api/send-email", (req, res) => {
 	try {
 		if (!req.body) throw new Error("No body");
 		console.log(req.body);
@@ -35,18 +31,6 @@ app.post("/send-email", (req, res) => {
 		console.log(err);
 		res.status(500).send("Email not sent");
 	}
-});
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
 });
 
 module.exports = app;
