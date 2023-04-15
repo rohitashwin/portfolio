4b1b2df1d79092a38a6afe316d9c7b897487d5db
Ashwin Rohit <ashwinrohit@proton.me>
2023-04-14 18:10:49 -0700

    Add Error Handler

--- api/send-email.js
+++ api/send-email.js
@@ -1,32 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = (req, res) => {
-	// Using mongodb for now, then will switch to postmark or some other email service
-	const mongodb_user = process.env.MONGODB_USER;
-	const mongodb_pass = process.env.MONGODB_PASS;
-	const mongodb_server = process.env.MONGODB_SERVER;
-	const url = `mongodb+srv://${mongodb_user}:${mongodb_pass}@${mongodb_server}`;
-	mongoose.set('strictQuery', false);
-	mongoose.connect(url);
-	const emailSchema = new mongoose.Schema({
-		subject: String,
-		from: String,
-		message: String,
-	});
-	const Email = mongoose.model('Email', emailSchema);
-	if (!req.body.name || !req.body.email || !req.body.message) {
-		res.status(400).send({ message: 'Missing required fields!' });
-		return;
+	try {
+		// Using mongodb for now, then will switch to postmark or some other email service
+		const mongodb_user = process.env.MONGODB_USER;
+		const mongodb_pass = process.env.MONGODB_PASS;
+		const mongodb_server = process.env.MONGODB_SERVER;
+		const url = `mongodb+srv://${mongodb_user}:${mongodb_pass}@${mongodb_server}`;
+		mongoose.set("strictQuery", false);
+		mongoose.connect(url);
+		const emailSchema = new mongoose.Schema({
+			subject: String,
+			from: String,
+			message: String,
+		});
+		const Email = mongoose.model("Email", emailSchema);
+		if (!req.body.name || !req.body.email || !req.body.message) {
+			res.status(400).send({ message: "Missing required fields!" });
+			return;
+		}
+		const email = new Email({
+			subject: req.body.name,
+			from: req.body.email,
+			message: req.body.message,
+		});
+		email.save().then(() => {
+			res.status(200).send({ message: "Email saved!" });
+		});
+		mongoose.connection.close();
+	} catch (err) {
+		res.status(500).send({ message: "Internal server error!" });
+		console.error(err);
 	}
-	const email = new Email({
-		subject: req.body.name,
-		from: req.body.email,
-		message: req.body.message,
-	});
-	email.save().then(() => {
-		res.status(200).send({ message: 'Email saved!' });
-	});
-	mongoose.connection.close();
 };
 
 module.exports = app;
