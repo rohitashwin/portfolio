2b2dbaaf3ea0e8e20e7a01a8536411ad5d476553
Ashwin Rohit <ashwinrohit@proton.me>
2023-04-14 17:55:12 -0700

    Replace nodemailer with MongoDB

--- api/send-email.js
+++ api/send-email.js
@@ -1,47 +1,28 @@
-import path from "path";
-const nodemailer = require("nodemailer");
+const mongoose = require('mongoose');
 
-// Configure email server
 const app = (req, res) => {
-	try {
-		if (!req.body) throw new Error("No body");
-
-		const pathToEmail = path.join(__dirname, "test.txt");
-		const fs = require("fs");
-		const filepath = pathToEmail;
-		const content = JSON.stringify(req.body);
-
-		fs.writeFile(filepath, content, (err) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			console.log("File written successfully!");
-		});
-
-		if (!process.env.EMAIL || !process.env.PASSWORD) {
-			throw new Error("No email or password");
-		}
-		const transporter = nodemailer.createTransport({
-			service: "gmail",
-			auth: {
-				user: process.env.EMAIL,
-				pass: process.env.PASSWORD,
-			},
-		});
-		const { name, email, message } = req.body;
-		const mailOptions = {
-			from: email,
-			to: process.env.EMAIL,
-			subject: `Message from ${name}`,
-			text: message,
-		};
-		transporter.sendMail(mailOptions);
-		res.status(200).send("Email sent");
-	} catch (err) {
-		console.log(err);
-		res.status(500).send("Email not sent");
-	}
+	// Using mongodb for now, then will switch to postmark or some other email service
+	const mongodb_user = process.env.MONGODB_USER;
+	const mongodb_pass = process.env.MONGODB_PASS;
+	const mongodb_server = process.env.MONGODB_SERVER;
+	const url = `mongodb+srv://${mongodb_user}:${mongodb_pass}@${mongodb_server}`;
+	mongoose.set('strictQuery', false);
+	mongoose.connect(url);
+	const emailSchema = new mongoose.Schema({
+		subject: String,
+		from: String,
+		message: String,
+	});
+	const Email = mongoose.model('Email', emailSchema);
+	const email = new Email({
+		subject: req.body.subject,
+		from: req.body.from,
+		message: req.body.message,
+	});
+	email.save().then(() => {
+		res.status(200).send({ message: 'Email saved!' });
+	});
+	mongoose.connection.close();
 };
 
 module.exports = app;
